089910a3c6862b2b3443bd5db6323fc87773b0f6
SohhnQuixote <ssmin1215@gmail.com>
2022-05-07 22:38:12 +0900

    baek
    solved

---
+++ C++/구현/개/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/*
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+*/
+int main(void)
+{
+  cout<<R"(|\_/|)"<<endl;
+  cout<<R"(|q p|   /})"<<endl;
+  cout<<"( 0 )"<<R"("""\)"<<endl;
+  cout<<R"(|"^"`    |)"<<endl;
+  cout<<R"(||_/=\\__|)"<<endl;
+    return 0;
+}
