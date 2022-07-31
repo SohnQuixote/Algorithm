a3a8758f2b6cd07ab1b79198d7d54f6627a04ae6
SohhnQuixote <ssmin1215@gmail.com>
2022-07-31 11:27:15 +0900

    baek
    solved

---
+++ C++/삼성 기출/파이프 옮기기 1/시간초과_1.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+vector<vector<int>> arr(16,vector<int>(16,0));
+int n;
+int dx[3] = {1,1,0};
+int dy[3] = {1,0,1};
+int cnt = 0;
+//dir = 0 가로 , dir = 1 세로, dir = 2 대각선
+void print_pipe()
+{
+  for(int i=0;i<n;i++)
+    {
+    for(int j=0;j<n;j++)
+      {
+        cout<<arr[i][j]<<' ';
+      }
+      cout<<endl;
+    }
+  
+}
+
+
+void place(int x, int y, int dir)
+{
+  //print_pipe();
+  if(x==n-1 && y==n-1)
+  {
+    cnt++;
+    return;
+    
+  }
+  int mov_x,mov_y;
+  vector<vector<int>> moving_prev = arr;
+  if(dir == 0)
+  {
+    //가로
+    mov_x = x+dx[2];
+    mov_y = y+dy[2];
+    if(mov_x>=0 && mov_x < n && mov_y>=0 && mov_y<n && arr[mov_x][mov_y] == 0)
+    {
+      arr[mov_x][mov_y] = 2;
+      place(mov_x,mov_y,0);
+    }
+    arr = moving_prev;
+
+    
+  }
+  else if(dir == 1)
+  {
+    //세로
+    mov_x = x+dx[1];
+    mov_y = y+dy[1];
+    if(mov_x>=0 && mov_x < n && mov_y>=0 && mov_y<n && arr[mov_x][mov_y] == 0)
+    {
+      arr[mov_x][mov_y] = 2;
+      place(mov_x,mov_y,1);
+    }
+    arr = moving_prev;
+    
+  }
+  else if(dir == 2)
+  {
+    mov_x = x+dx[2];
+    mov_y = y+dy[2];
+    if(mov_x>=0 && mov_x < n && mov_y>=0 && mov_y<n && arr[mov_x][mov_y] == 0)
+    {
+      arr[mov_x][mov_y] = 2;
+      place(mov_x,mov_y,0);
+    }
+    arr = moving_prev;
+
+
+    mov_x = x+dx[1];
+    mov_y = y+dy[1];
+    if(mov_x>=0 && mov_x < n && mov_y>=0 && mov_y<n && arr[mov_x][mov_y] == 0)
+    {
+      arr[mov_x][mov_y] = 2;
+      place(mov_x,mov_y,1);
+    }
+    arr = moving_prev;
+
+    
+    
+  }
+
+  bool place_all_flag = true;
+    for(int d=0;d<3;d++)
+      {
+        mov_x = x+dx[d];
+        mov_y = y+dy[d];
+        if(mov_x>=0 && mov_x <n && mov_y >= 0 && mov_y<n && arr[mov_x][mov_y] == 0)
+        {
+          arr[mov_x][mov_y] = 2;
+        }
+        else
+        {
+          place_all_flag = false;
+        }
+      }
+    if(place_all_flag)
+    {
+      place(x+1,y+1,2);
+    }
+    arr = moving_prev;
+  
+}
+
+
+
+int main() {
+  cin>>n;
+  for(int i=0;i<n;i++)
+    for(int j=0;j<n;j++)
+      cin>>arr[i][j];
+  arr[0][0] = 2;
+  arr[0][1] = 2;
+  //처음 가로로 되어 있는 상태
+  place(0,1,0);
+  cout<<cnt<<'\n';
+  
+}
