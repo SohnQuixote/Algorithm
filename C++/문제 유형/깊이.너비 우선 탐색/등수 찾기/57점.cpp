fef029444b9bef6b288f28b23ae51c9a214874c3
SohhnQuixote <ssmin1215@gmail.com>
2022-08-01 13:34:59 +0900

    baek
    solved

---
+++ C++/문제 유형/깊이.너비 우선 탐색/등수 찾기/57점.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,m,x;
+//정방향 그래프, 반대방향 그래프
+vector<int> graphs[2][100000];
+int highest = 1,lowest = 1;
+int visited[100000] = {0,};
+void find_lowest(int now)
+{
+  if(graphs[0][now].size() ==0)
+  {
+    return;
+  }
+  for(int i=0;i<graphs[0][now].size();i++)
+    {
+      if(!visited[graphs[0][now][i]])
+      {
+        visited[graphs[0][now][i]] = 1;
+        lowest++;
+        find_lowest(graphs[0][now][i]);
+        
+      }
+      
+    }
+  
+}
+void find_highest(int now)
+{
+  if(graphs[1][now].size() == 0)
+  {
+    return;
+  }
+    for(int i=0;i<graphs[1][now].size();i++)
+    {
+      if(!visited[graphs[1][now][i]])
+      {
+        visited[graphs[1][now][i]] = 1;
+        highest++;
+        find_highest(graphs[1][now][i]);
+        
+      }
+      
+    }
+
+  
+}
+
+
+int main() {
+    cin>>n>>m>>x;
+    for(int i=0;i<m;i++)
+      {
+        int high,low;
+        cin>>high>>low;
+        graphs[0][high].push_back(low);
+        graphs[1][low].push_back(high);
+      }
+  //순환그래프 아님
+  find_highest(x);
+  for(int i=0;i<n;i++)
+    visited[i] = 0;
+  find_lowest(x);
+  cout<<highest<<' ';
+  cout<<n-lowest +1 <<endl;
+  // low->high 멤버수 + 1  가능한 가장 높은 등수
+  //인원수 - high-> low +  1(자신) => 가능한 가장 낮은 등수
+}
