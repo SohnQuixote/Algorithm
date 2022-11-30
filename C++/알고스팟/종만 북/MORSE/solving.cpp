e1263b368c58aa8f0f5d1a5cba59f34b2326bfe8
SohhnQuixote <ssmin1215@gmail.com>
2022-11-30 21:56:12 +0900

    jongman
    solving

---
+++ C++/알고스팟/종만 북/MORSE/solving.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*
+  MORSE
+  n개의 장점 m개의 단점
+  - ascii  code 45
+  o ascii code 111
+  장점 n, 단점 m, k 번째 신호 
+  1. 고려사항 dp를 어디에?
+  선택지는 두개
+  근데 -를먼저해야됨
+  그리고 몇번째지?
+  사실 부분문제임
+
+  cache를 어디다가???
+  n과 m에 따라서 다른 값이 나오는 것이 문제임
+
+*/
+string(int long_num,int short_num,int num)
+{
+  //하나씩 빼가면서 더함
+  
+
+
+  
+}
+
+
+
+
+int main() {
+  int C;
+  cin>>C;
+  for(int i=0;i<C;i++)
+    {
+      int n,m,k;
+      cin>>n>>m>>k;
+      
+
+
+      
+    }
+}
