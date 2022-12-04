74cd84030c9b88f61103a8f5c038ca26adf86bc9
SohhnQuixote <ssmin1215@gmail.com>
2022-12-04 20:54:09 +0900

    jongma
    solving

---
+++ C++/알고스팟/종만 북/KLIS/solving.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/*
+  KLIS
+
+  단조 증가 할때 
+  이중 가장 긴것을 LIS
+  LIS 두개 이상 가능
+  중복 숫자 없는 수열 주어질 때 사전 순서대로 맨 앞에서 K번째 있는 LIS 구하기
+  
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
