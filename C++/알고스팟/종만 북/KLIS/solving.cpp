cb8cf11e75bcf57dd4b4b020ff27faa59f497aba
SohhnQuixote <ssmin1215@gmail.com>
2022-12-19 22:58:18 +0900

    solving

--- C++/알고스팟/종만 북/KLIS/solving.cpp
+++ C++/알고스팟/종만 북/KLIS/solving.cpp
@@ -7,8 +7,23 @@ using namespace std;
   이중 가장 긴것을 LIS
   LIS 두개 이상 가능
   중복 숫자 없는 수열 주어질 때 사전 순서대로 맨 앞에서 K번째 있는 LIS 구하기
+  모든 숫자가 서로 다른 수열이 주어짐
+  이 수열중 사전순서대로 맨앞에서 K번째에 있는 LIS를 구하기
+  LIS는 어떻게 구했지?
+  -> 재귀 호출 -> 동적 계획법
   
 */
+int C;
+int N,K;
+
+int arr[500];
 int main() {
-  std::cout << "Hello World!\n";
+  cin>>C;
+  for(int tc=0;tc<C;tc++)
+    {
+      cin>>N>>K;
+      for(int i=0;i<N;i++)
+        cin>>arr[i];
+      
+    }
 }
