bde6bcde80d3efd6165b1865c3b67e0441ce02a2
SohhnQuixote <ssmin1215@gmail.com>
2023-01-02 01:42:30 +0900

    jongman
    solving

---
+++ C++/알고스팟/종만 북/DRAGON/solving.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/*
+  DRAGON
+  0세대 FX
+  X => X+YF
+  Y => FX-Y
+  1세대 FX+YF
+  2세대 FX+YF + FX-YF
+  문자열중 p번째 글자부터 l글자만을 계산
+  할 때 전부다 해야하는것인가?
+  너무 길긴함
+  어쨌든 다봐야 되긴 함
+  규칙 자체는 간단
+  함수 인자로 p와 l을 넘겨야함
+  n세대와
+*/
+int main() {
+  std::cout << "Hello World!\n";
+}
